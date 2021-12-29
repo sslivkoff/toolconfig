1db393e945fbb1987ea35c6f3f724ee3e37b2e0f
sslivkoff <stormslivkoff@gmail.com>
2021-12-28 17:15:45 -0800

    consolidate types in spec.py

--- toolconfig/spec.py
+++ toolconfig/spec.py
@@ -1,2 +1,19 @@
+import typing
+
+
 ConfigSpec = dict
+ConfigData = typing.MutableMapping[str, typing.Any]
+
+ValidationOption = typing.Literal['raise', 'warn', False]
+
+
+class GetConfigKwargs(typing.TypedDict, total=False):
+    config_path_env_var: typing.Optional[str]
+    config_path: typing.Optional[str]
+    default_config_path: typing.Optional[str]
+    config_spec: typing.Optional[ConfigSpec]
+    default_config_values: typing.Optional[dict]
+    config_required: bool
+    config_variables: typing.Optional[dict[str, typing.Mapping]]
+    validate: typing.Literal['raise', 'warn', False]
 
