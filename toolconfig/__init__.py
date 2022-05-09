62a55d4b3e4eb6cf2ae8f472ac79502501538423
sslivkoff <stormslivkoff@gmail.com>
2022-05-08 22:00:02 -0700

    upgrade from setup.py to pyproject.toml

--- toolconfig/__init__.py
+++ toolconfig/__init__.py
@@ -1,4 +1,9 @@
+"""toolconfig provides tools for loading and managing config files"""
+
 from .crud import *
 from .filesystem import *
 from .spec import *
 from .validation import *
+
+
+__version__ = '0.1.0'
